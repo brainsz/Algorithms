ae2e6a5489682563f072c2f3a6425dc4c085c252
brains <brains_z@163.com>
2017-05-06 14:54:37 +0800

    modify 35
    modify 35

--- leetcode/35 Search Insert Position.js
+++ leetcode/35 Search Insert Position.js
@@ -1,49 +1,50 @@
 /*
-35. Search Insert Position
+ 35. Search Insert Position
 
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-You may assume no duplicates in the array.
+ You may assume no duplicates in the array.
 
-Here are few examples.
-[1,3,5,6], 5 → 2
-[1,3,5,6], 2 → 1
-[1,3,5,6], 7 → 4
-[1,3,5,6], 0 → 0
-*/
-
-var searchInsert = function(nums, target) {
-	//第一种解法 从后面往前面找,O(n)
-	/*if(nums==null){
-		return null;
-	}
-	var k=0;
-	if(nums.length==0){
-		return k;
-	}
-    if(target>nums[nums.length-1]){
+ Here are few examples.
+ [1,3,5,6], 5 → 2
+ [1,3,5,6], 2 → 1
+ [1,3,5,6], 7 → 4
+ [1,3,5,6], 0 → 0
+ */
+//分治法解决
+//1.第一种解法从后往前寻找，这种方法需要O(n)复杂度，但是如果对于需要插入的元素比较好
+var searchInsert = function (nums, target) {
+    if (nums == null) {
+        return null;
+    }
+    var k = 0;
+    if (nums.length == 0) {
+        return k;
+    }
+    if (target > nums[nums.length - 1]) {
         return nums.length;
     }
-    for(var i=nums.length-1;i>=0;i--){
-    	if(nums[i]>=target){
-    		k=i;
-    	}
+    for (var i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] >= target) {
+            k = i;
+        }
     }
-    return k;*/
-	//第二种二分查找
-	var left=0,
-		right=nums.length-1;
-	while(left<right){
-		var mid=left+((right-left)>>1);
-		//console.log(mid);
-		if(nums[mid]==target){
-			return mid;
-		}else if(nums[mid]<target){
-			left=mid+1;
-		}else{
-			right=mid-1;
-		}
-	}
-	return nums[left]>=target?left:left+1;
+    return k;
 };
-searchInsert([1,3,5,6,7,8,12,25],26);
+//第二种二分查找O(log^n)
+var searchInsert = function (nums, target) {
+    var left = 0,
+        right = nums.length - 1;
+    while (left < right) {
+        var mid = left + ((right - left) >> 1);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return nums[left] >= target ? left : left + 1;
+};
+searchInsert([1, 3, 5, 6, 7, 8, 12, 25], 26);
