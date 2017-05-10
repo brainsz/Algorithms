cde72000210afb5cc5a7625027c850a9207e8879
brains <brains_z@163.com>
2017-05-10 22:29:02 +0800

    Find Minimum in Rotated Sorted Array II
    Find Minimum in Rotated Sorted Array II

--- leetcode/154. Find Minimum in Rotated Sorted Array II.js
+++ leetcode/154. Find Minimum in Rotated Sorted Array II.js
@@ -1,33 +1,42 @@
 /**
  * Created by brains on 2017/5/10.
  */
+
 /**
- * 153. Find Minimum in Rotated Sorted Array
- *
+ * 154. Find Minimum in Rotated Sorted Array II
  * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
  (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 
  Find the minimum element.
+
+ The array may contain duplicates
  *
  * */
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
-    //用二分查找
+
+    /**
+     *
+     * 当A[mid] = A[end]时，无法判断min究竟在左边还是右边。
+
+     但可以肯定的是可以排除A[end]：因为即使min = A[end]，由于A[end] = A[mid]，排除A[end]并没有让min丢失。所以增加的条件是：
+     A[mid] = A[end]：搜索A[start : end-1]
+     *
+     * */
 var findMin = function(nums) {
     var left=0,
         right=nums.length-1;
     while(left<right){
         var mid=left+((right-left)>>1);
-        //如果中间的数字小于右边的，则右边等于中间
         if(nums[mid]<nums[right]){
             right=mid;
-        }else{
-            //否则左边等于中间+1
+        }else if(nums[mid]>nums[right]){
             left=mid+1;
+        }else{
+            right--;
         }
     }
     return nums[left];
