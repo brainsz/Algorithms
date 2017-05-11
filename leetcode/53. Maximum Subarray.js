b77558f5ea10c3f10e86a642f0edd82f9e8cacc2
brains <brains_z@163.com>
2017-05-11 23:01:17 +0800

    Maximum Subarray
    Maximum Subarray

---
+++ leetcode/53. Maximum Subarray.js
@@ -0,0 +1,25 @@
+/**
+ * Created by brains on 2017/5/10.
+ */
+/**
+ * 53. Maximum Subarray
+ *
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ *
+ * */
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    var sum=0;
+    for(var i=0;i<nums.length;i++){
+        if(nums[i]<0){
+        }
+    }
+};
