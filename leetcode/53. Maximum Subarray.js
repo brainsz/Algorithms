66dcbdd999f65f85e3dff0fe379be3fe375e686d
brains <brains_z@163.com>
2017-05-15 21:19:47 +0800

    Maximum Subarray
    Maximum Subarray

--- leetcode/53. Maximum Subarray.js
+++ leetcode/53. Maximum Subarray.js
@@ -16,10 +16,26 @@
  * @param {number[]} nums
  * @return {number}
  */
+    //假设nums[i]之前的连续段的和为sum
+    //1.如果sum>=0,就可以和nums[i]拼接在一起构成新的sum，加上一个正数肯定更大
+    //2.如果sum<0,就不用和nums[i]拼接在一起了，不管nums[i]多小，加上一个负数肯定更销售
+    //但是因为需要求连续的数组元素，所有需要把这个连续的数值和全局记录的最大值进行比较
+    //
 var maxSubArray = function(nums) {
-    var sum=0;
+    var sum=0,
+        max_sum=Number.MIN_VALUE;
     for(var i=0;i<nums.length;i++){
-        if(nums[i]<0){
+        if(sum>=0){
+            sum+=nums[i];
+        }else{
+            //把当前值赋值给sum，重新开始计数
+            sum=nums[i];
+        }
+        //如果sum大于当前全局最大值，则赋值给max_sum
+        if(sum>max_sum){
+            max_sum=sum;
         }
     }
+    return max_sum;
 };
+console.log(maxSubArray([-1]));
