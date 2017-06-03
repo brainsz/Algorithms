0bdd6aaf7807550c8b71af1dc3668c8dab5ca8c2
brains <brains_z@163.com>
2017-06-03 23:25:31 +0800

    binaryTree
    binaryTree

---
+++ basicAlgorithms/binaryTree.js
@@ -0,0 +1,133 @@
+/**
+ * Created by brains on 2017/6/3.
+ */
+/**
+ * 二叉树遍历
+ *
+ * */
+
+
+//链式存储结构,节点对象的构造函数
+function BinaryTree(data,left,right){
+    this.data=data||null;
+    this.left=left||null;
+    this.right=right||null;
+}
+//插入节点，构造二叉树
+BinaryTree.prototype.insert=function(data){
+    var n=new BinaryTree(data,null,null);
+    //是否根节点已经构造完成
+    if(this.root==null){
+        this.root=n;
+    }else{
+        var current=this.root;
+        var parent;
+        while(true){
+            //当前节点设置为父节点
+            parent=current;
+            //如果小于当前根结点的值,则应该插入到左子树
+            if(data<current.data){
+                //当前节点设置为左子树
+                current=current.left;
+                if(current==null){
+                    parent.left=n;
+                    break;
+                }
+            }else{
+                current=current.right;
+                if(current==null){
+                    parent.right=n;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+//中序遍历二叉树
+function inOrder(node){
+    if(!(node==null)){
+        inOrder(node.left);
+        console.log(node.data);
+        inOrder(node.right);
+    }
+}
+//非递归深度优先遍历二叉树
+function inOrderDFS(root){
+    if(!root){
+        return null;
+    }
+    var arr=[];
+    arr.push(root);
+    while(arr.length!==0){
+        var node=arr.pop();
+        console.log(node.data);
+        if(node.right){
+            arr.push(node.right);
+        }
+        if(node.left){
+            arr.push(node.left);
+        }
+
+    }
+}
+//非递归广度优先遍历二叉树
+function inOrderBFS(root){
+    if(root===null){
+        console.error("root is null")
+    }
+    let arr=[];
+    arr.push(root);
+    while(arr.length!==0){
+        //从第一个元素开始弹出
+        let node=arr.shift();
+        console.log(node.data);
+        if(node.left){
+            arr.push(node.left);
+        }
+        if(node.right){
+            arr.push(node.right)
+        }
+    }
+}
+
+//非递归中序遍历
+function inOrderStack(root){
+    if(root===null){
+        console.log("root is null");
+    }
+    var arr=[];
+    arr.push(root)
+   //console.log(arr);
+    var p=root;
+    while(p!==null&&arr.length!==0){
+        //左边一直走到头
+        while(p){
+            p=p.left;
+            if(p!==null){
+                arr.push(p);
+            }
+        }
+        if(arr.length){
+            p=arr.pop();
+            console.log(p.data);
+            arr.push(p.right);
+            p=p.right;
+        }
+    }
+}
+
+
+var root=null;
+//插入元素
+var nums=new BinaryTree(root);
+//将以下数据导入二叉树
+nums.insert(23)
+nums.insert(45)
+nums.insert(16)
+nums.insert(37)
+nums.insert(3)
+nums.insert(99)
+nums.insert(22)
+
+inOrderStack(nums.root)
---
+++ leetcode/513. Find Bottom Left Tree Value.js
@@ -0,0 +1,32 @@
+/**
+ * Created by brains on 2017/6/3.
+ */
+/**
+ * 513. Find Bottom Left Tree Value
+ *
+ * Given a binary tree, find the leftmost value in the last row of the tree
+ *
+ * */
+
+/**
+ * Definition for a binary tree node.
+ *
+ *
+ *
+ *
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function TreeNode(val){
+    this.val=val;
+    this.left=this.right=null;
+}
+
+var findBottomLeftValue = function(root) {
+    if(root==null){
+        return null;
+    }
+
+};
