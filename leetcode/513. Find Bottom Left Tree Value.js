430a3645d35ffac13e2778044ba845f74cdae63d
brains <brains_z@163.com>
2017-06-04 12:02:54 +0800

    513. Find Bottom Left Tree Value
    513. Find Bottom Left Tree Value

--- leetcode/513. Find Bottom Left Tree Value.js
+++ leetcode/513. Find Bottom Left Tree Value.js
@@ -25,8 +25,26 @@ function TreeNode(val){
 }
 
 var findBottomLeftValue = function(root) {
-    if(root==null){
-        return null;
+    if(root===null){
+        console.error("root is null")
     }
-
+    let arr=[];
+    arr.push(root);
+    var result;
+    while(arr.length!==0){
+        var size=arr.length;
+        //保存每一层最左边的值
+        result=arr[0].val;
+        while(size--){
+            //从第一个元素开始弹出
+            let node=arr.shift();
+            if(node.left){
+                arr.push(node.left);
+            }
+            if(node.right){
+                arr.push(node.right)
+            }
+        }
+    }
+    return result;
 };
